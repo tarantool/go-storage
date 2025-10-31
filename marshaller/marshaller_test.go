1dce6f3f41766069a2ca969214b8e69235e4a3ce
Alexey Potapenko <a.potapenko@vkteam.ru>
2025-10-31 08:20:16 +0300

    integrity: signer and verifier implementation
    Closes #TNTP-4171

---
+++ marshaller/marshaller_test.go
@@ -0,0 +1,59 @@
+package marshaller_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/tarantool/go-storage/marshaller"
+)
+
+type YamlStruct struct {
+	Title string `yaml:"title"`
+	Link  string `yaml:"link"`
+}
+
+func TestMarshal(t *testing.T) {
+	t.Parallel()
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The test did not panic")
+		}
+	}()
+
+	marshaller := marshaller.NewYamlMarshaller()
+
+	data, err := marshaller.Marshal([]byte{123, 123})
+	require.NoError(t, err)
+	require.NotNil(t, data)
+
+	ch := make(chan int)
+
+	data, err = marshaller.Marshal(ch)
+	require.Error(t, err)
+	require.Nil(t, data)
+}
+
+func TestUnmarshal(t *testing.T) {
+	t.Parallel()
+
+	marshaller := marshaller.NewYamlMarshaller()
+
+	var unmarshaledYaml YamlStruct
+
+	validYaml := `
+title: "Link"
+link: "https://some.link"
+`
+
+	err := marshaller.Unmarshal([]byte(validYaml), &unmarshaledYaml)
+	require.NoError(t, err)
+
+	invalidYaml := `
+TITLE: 123
+     Link: true
+`
+
+	err = marshaller.Unmarshal([]byte(invalidYaml), &unmarshaledYaml)
+	require.Error(t, err)
+}
